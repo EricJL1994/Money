ec2f3bbc72e716f448f095a9668f494ae6630b47
usuario <usuario@10.22.143.27>
2014-11-18 14:25:59 +0000

    Con la kata5

--- src/ui/ExchangeDialog.java
+++ src/ui/ExchangeDialog.java
@@ -1,8 +1,6 @@
 package ui;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import model.Currency;
 import model.CurrencyMap;
 import model.Exchange;
@@ -11,41 +9,7 @@ import model.Money;
 public class ExchangeDialog {
 
     public Exchange execute(CurrencyMap currencyMap) throws IOException {
-        System.out.println("Seleccione una divisa de la lista");
-        System.out.println(currencyMap.toString());
-        System.out.println("Introduce el código de la divisa origen");
-        Currency moneyCurrency = readCurrency(currencyMap);
-        System.out.println("Introduce el código de la divisa destino");
-        Currency currency = readCurrency(currencyMap);
-        System.out.println("Introduce la cantidad para cambiar");
-        double amount = readAmount();
-        return new Exchange(new Money(moneyCurrency, amount), currency);
-    }
-    
-    private Currency readCurrency(CurrencyMap currencyMap) throws IOException{
-        InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(isr);
-        Currency currency;
-        String cadena;
-        do{
-            System.out.print("Código: ");
-            cadena = br.readLine();
-        }while((currency = currencyMap.findCurrency(cadena)) == null);
-        
-        return currency;
-
-    }
-    
-    private double readAmount() throws IOException{
-        InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(isr);
-        double amount = 0;
-        String cadena;
-        do{
-            System.out.print("Cantidad: ");
-            cadena = br.readLine();
-            amount = Double.parseDouble(cadena);
-        }while(amount == Double.NaN);
-        return amount;
+        return null;
+        //return new Exchange(new Money(moneyCurrency, amount), currency);
     }
 }
